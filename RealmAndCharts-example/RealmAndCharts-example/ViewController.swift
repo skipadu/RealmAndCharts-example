69606058bf2cc7afb0d0bb6f91790057ddc00da0
Sami Korpela <apps@pihrit.com>
2016-10-08 11:30:09 +0300

    Initial commit
    Created the project

---
+++ RealmAndCharts-example/RealmAndCharts-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmAndCharts-example
+//
+//  Created by Sami Korpela on 8.10.2016.
+//  Copyright © 2016 skipadu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
